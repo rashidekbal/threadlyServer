3186e2a859bef933a2be66526c94e329b1291dc2
Rasid Ekbal <rtechdevlopment123@gmail.com>
2026-01-04 04:34:04 +0530

    add db port

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import mysql from "mysql2";
-let host = process.env.HOST;
-let user = process.env.USER;
-let password = process.env.PASSWORD;
+let host = process.env.DB_HOST;
+let user = process.env.DB_USER;
+let password = process.env.DB_PASSWORD;
 let database = process.env.DB;
+let port=process.env.DB_PORT;
 
 let connection = mysql.createConnection({
+  port,
   host,
   user,
   password,
